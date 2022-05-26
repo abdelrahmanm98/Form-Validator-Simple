2854aa5c361e86f9e13e78cafdaeaab956062612
abdelrahmanm98 <abdo62771@gmail.com>
2022-05-26 05:36:39 +0200

    Second update javascript

---
+++ script.js
@@ -0,0 +1,50 @@
+const form = document.querySelector("#form");
+const username = document.querySelector("#username");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const password2 = document.querySelector("#password2");
+
+// Show input error message
+function showError(input, message) {
+  // move to the parent elment = form-control
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+// show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+// Check email is valid
+function isValidEmail(email) {
+  const regular =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regular.test(String(email).toLowerCase());
+}
+
+// Chech required field
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+// Get fieldname
+function getFieldName(input) {
+  // charAt >> get first letter (lowercase) + input.id (- minus the first letter )
+  return input.id.charAt(0).toLowerCase() + input.id.slice(1);
+}
+
+// Event listeners
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  checkRequired([username, email, password, password2]);
+});
